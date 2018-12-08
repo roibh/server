779153dee02b6281bfb8d5709bde369f33497661
Roi ben haim (rb136m) <rb136m@att.com>
2018-12-08 02:17:10 +0200

    no message

--- index.ts
+++ index.ts
@@ -4,11 +4,14 @@ import {
     ClientConfiguration, ConfiguredServer, MethodType, ServerType,
 } from '@methodus/server';
 
+
+
 export * from './models/';
 import * as path from 'path';
 import { DBHandler } from '@methodus/data';
 import { configuration } from './db/config';
 import { AuthController, UserController, ScreensDataController } from './controllers/';
+import { Playlist } from './controllers/playlist.data.controller';
 
 DBHandler.config = configuration;
 
@@ -17,6 +20,7 @@ DBHandler.config = configuration;
 @PluginConfiguration(path.join(__dirname, 'static'), { path: '/', clientPath: '/public' })
 @ClientConfiguration(UserController, MethodType.Local, ServerType.Express)
 @ClientConfiguration(AuthController, MethodType.Local, ServerType.Express)
+@ClientConfiguration(Playlist, MethodType.Local, ServerType.Express)
 @ClientConfiguration(ScreensDataController, MethodType.Local, ServerType.Express)
 class SetupServer extends ConfiguredServer {
     constructor() {
