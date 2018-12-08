6cdd87e71564faafd32f23ad77f9dc202cc5138d
Roi ben haim (rb136m) <rb136m@att.com>
2018-12-08 02:37:48 +0200

    no message

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports.Config = {
     connections: {
         'default': {
-            server: `mongodb://${process.env.MONGO_NAME}:${process.env.MONGO_PASSWORD}@process.env.MONGO_PATH`,
+            server: `mongodb://${process.env.MONGO_NAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_PATH}`,
             db: 'signnature',
             poolSize: 10,
             ssl: false,
