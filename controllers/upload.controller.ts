4ffe6452e9400e1a9271042a4d2d74e16c0e9827
Roi ben haim (rb136m) <rb136m@att.com>
2019-02-21 01:18:50 +0200

    no message

--- controllers/upload.controller.ts
+++ controllers/upload.controller.ts
@@ -9,7 +9,7 @@ import * as uuidv1 from 'uuid/v1';
 import * as fs from 'fs';
 
 const S3_BUCKET = process.env.S3_BUCKET;
-const S3_BUCKET_MIN = process.env.S3_BUCKET_MIN;
+// const S3_BUCKET_MIN = process.env.S3_BUCKET_MIN;
 const S3_REGION = 'us-east-2';
 
 const s3 = new aws.S3({ signatureVersion: 'v4', region: 'us-east-2' });
@@ -31,16 +31,17 @@ export class Upload {
             files.forEach((file) => {
                 const dateKey = moment().format('MM_YYYY');
 
-                let ActiveBucket = S3_BUCKET_MIN;
+                const ActiveBucket = S3_BUCKET;
                 // grab extension
-                const arr = file.name.split('.');
-                const ext = arr[arr.length - 1];
-                const resizeArr = ['jpg', 'png', 'gif' ];
-                if (resizeArr.indexOf(ext) === -1) {
-                    ActiveBucket = S3_BUCKET;
-                }
+                // const arr = file.name.split('.');
+                // const ext = arr[arr.length - 1];
+                // const resizeArr = ['jpg', 'png', 'gif' ];
+                // if (resizeArr.indexOf(ext) === -1) {
+                //     ActiveBucket = S3_BUCKET;
+                // }
 
                 const persist_filename = securityContext._id + '/' + dateKey + '/' + `${uuidv1()}_${file.name}`;
+
                 const s3Params = {
                     ACL: 'public-read',
                     Bucket: S3_BUCKET,
@@ -48,29 +49,29 @@ export class Upload {
                     // Expires: 60,
                     Key: persist_filename,
                 };
+                // s3.getSignedUrl('getObject', s3Params, (rawErr: any, rawData: any) => {
+                //     const s3ThumbParams = {
+                //         ACL: 'public-read',
+                //         Bucket: ActiveBucket,
+                //         ContentType: file.mimetype,
+                //         // Expires: 60,
+                //         Key: persist_filename,
+                //     };
                 s3.getSignedUrl('getObject', s3Params, (rawErr: any, rawData: any) => {
-                    const s3ThumbParams = {
-                        ACL: 'public-read',
-                        Bucket: ActiveBucket,
-                        ContentType: file.mimetype,
-                        // Expires: 60,
-                        Key: persist_filename,
+                    const returnData = {
+                        signedRequest: rawData,
+                        url: `https://s3.${S3_REGION}.amazonaws.com/${S3_BUCKET}/${persist_filename}`,
                     };
-                    s3.getSignedUrl('getObject', s3ThumbParams, (thumberr: any, thumbData: any) => {
-                        const returnData = {
-                            signedRequest: rawData,
-                            thumb: `https://s3.${S3_REGION}.amazonaws.com/${ActiveBucket}/${persist_filename}`,
-                            thumbSignedRequest: thumbData,
-                            url: `https://s3.${S3_REGION}.amazonaws.com/${S3_BUCKET}/${persist_filename}`,
-                        };
-                        resolve(returnData);
-                        const s3obj = new aws.S3({ params: s3Params });
-                        s3obj.upload({ Body: file.data } as any).
-                            send((err: any, data: any) => {
-                                // tslint:disable-next-line:no-console
-                                console.log(err, data);
-                            });
-                    });
+                    const s3obj = new aws.S3({ params: s3Params });
+                    s3obj.upload({ Body: file.data } as any).
+                        send((err: any, data: any) => {
+                            // tslint:disable-next-line:no-console
+                            console.log(err, data);
+
+                            resolve(returnData);
+                        });
+
+                    // });
                 });
             });
         });
